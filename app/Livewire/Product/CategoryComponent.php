0b2fba4b3aed7ed9085d5c936516f94726b8c190
alex <alex.sanders@ukr.net>
2025-08-19 22:40:50 +0300

    Enhance category page with Livewire pagination and loading UI
    Added Livewire's WithPagination and CartTrait to CategoryComponent for dynamic pagination and cart features. Introduced a loading overlay for improved UX during updates, updated CSS for the loading state, and replaced static pagination with Livewire's pagination links in the category Blade view. Added default Livewire configuration file.

--- app/Livewire/Product/CategoryComponent.php
+++ app/Livewire/Product/CategoryComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire\Product;
 
+use App\Helpers\Traits\CartTrait;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class CategoryComponent extends Component
 {
+
+    use WithPagination;
+    use CartTrait;
+
     public string $slug = '';
 
     public function mount(string $slug)
